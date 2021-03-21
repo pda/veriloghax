c23059ea2c920b4b77b2001d020077faefb76724
Paul Annesley <paul@annesley.cc>
2021-03-21 14:14:16 +1100

    Test bench reads status register from SPI flash EEPROM

--- hello_tb.v
+++ hello_tb.v
@@ -1,32 +1,51 @@
+`include "models/25AA512.v"
+
+`timescale 1us/1us
+
 module hello_tb;
 
-  reg clk, rst;
-  reg [3:0] x;
+  reg flash_si = 0;
+  wire flash_so;
+  reg flash_sck = 0; // rising:MOSI, falling:MISO
+  reg flash_cs_n = 1;
+  reg flash_wp_n = 1;
+  reg flash_hold_n = 1;
+  reg flash_reset = 0;
+
+  reg [7:0] shift = 0;
+
+  M25AA512 flash (
+    .SI(flash_si),
+    .SO(flash_so),
+    .SCK(flash_sck),
+    .CS_N(flash_cs_n),
+    .WP_N(flash_wp_n),
+    .HOLD_N(flash_hold_n),
+    .RESET(flash_reset)
+  );
 
   initial begin
-    $display("hello verilog");
-    // $monitor("[%4t] clk=%b x=%b", $time, clk, x);
-    $monitor("[t:%4t] x=%b", $time, x);
-
+    //$monitor("t:%1t shift:%b", $time, shift);
+    $monitor("[t:%4t] CS':%b SCK:%b SI:%b SO:%b shift:%08b", $time, flash_cs_n, flash_sck, flash_si, flash_so, shift);
     $dumpfile("hello.vcd");
     $dumpvars(0,hello_tb);
-  end
-
-  // clock & reset
-  initial begin
-    clk = 1'b1;
-    rst = 1'b1;              // reset
-    repeat(8) #1 clk = ~clk; // run a few clocks with reset held
-    rst = 1'b0;              // release reset
-    forever #1 clk = ~clk;   // run the clock
+    $readmemh("data.txt", flash.MemoryBlock, 0, 3);
   end
 
   initial begin
-    x = 0;
-    @(negedge rst);
-    repeat(32) @(posedge clk) begin
-      x = x + 1;
+    #100 // tPUP (power-up)
+    flash_cs_n = 0;
+    #100 // tCSS (CS setup time)
+    shift = 8'b0000_0101; // RDSR
+    repeat(8 + 8 - 1) begin // 8 SI + 8 SO - 1 shared clock
+      flash_si = shift[7];
+      flash_sck = 1;
+      #1 flash_sck = 0;
+      #1 shift = (shift << 1) | flash_so;
     end
+
+    #1 $display("STATUS: %b", shift);
+
     $finish;
   end
 
